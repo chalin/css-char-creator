f8729894ad532efd16b4476160cde9e6bfeb5f81
Patrice Chalin <pchalin@gmail.com>
2025-07-21 07:52:13 -0400

    POC for use of SCSS

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${path.resolve(__dirname, 'src/styles/_variables.scss')}";`
+      }
+    }
+  }
 });
